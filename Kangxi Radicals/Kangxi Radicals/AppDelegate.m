11edf718bbef4201182285dc69b62d9c326975f8
Sjors Provoost <sjors@sprovoost.nl>
2013-10-08 13:48:16 +0200

    Use bundle ID, don’t compile import_lite.m.

--- Kangxi Radicals/Kangxi Radicals/AppDelegate.m
+++ Kangxi Radicals/Kangxi Radicals/AppDelegate.m
@@ -17,7 +17,7 @@
 #import "RadicalsCharactersViewController.h"
 
 
-#define DO_IMPORT YES
+//#define DO_IMPORT YES
 
 #define MIXPANEL_TOKEN @"205327faee0efccec16b37c2db34d98d"
 
@@ -38,16 +38,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-//#ifdef DEBUG
-//    // Test randomness:
-//    for(int i=0; i < 10; i++) {
-//        NSInteger length = (unsigned long)10;
-//        
-//        NSInteger rand = (unsigned long)(random() % length);
-//        NSLog(@"Random: %lu", rand);
-//    }
-//#endif
-    
 #ifndef DEBUG
     [Mixpanel sharedInstanceWithToken:MIXPANEL_TOKEN];
     #ifdef LITE
@@ -124,118 +114,7 @@
 #ifndef DEBUG
         [[Mixpanel sharedInstance] track:@"Audio Session Error" properties:@{@"Method" : @"Activate Session",  @"Error" : [activationError description]}];
 #endif
-    }
-    
-
-
-
-    
-//    NSError *err;
-//    NSUInteger count = [self.managedObjectContext countForFetchRequest:request error:&err];
-//    if(count == NSNotFound) {
-//        //Handle error
-//    }
-    
-//    
-//    if(count == 0) {        // Dummy data:
-//        [Populator import:self.managedObjectContext];
-//        int sectionTally = 0;
-//        for(NSArray *section in @[
-//            @[@"人", @"口",@"亻",@"土",@"扌",@"日", @"月", @"木", @"氵",@"艹",@"讠",@"宀",@"又" ,@"禾" ,@"田" ,@"十" ,@"亠" ,@"厶" ,@"大" ,@"丷"],
-//            @[@"女", @"心", @"纟", @"辶", @"贝", @"目", @"尸", @"工", @"王", @"戈", @"匕", @"夂", @"巳", @"乚", @"冂", @"卜", @"𠃌", @"⺈"],
-//            @[@"忄", @"钅", @"刂", @"页", @"巾", @"山", @"攵", @"彳", @"方", @"儿", @"冫", @"子", @"斤", @"白", @"皿", @"干", @"夕", @"止", @"阝", @"ハ"]
-//        ]) {
-//            int tally = 0;
-//
-//            for(NSString *radical in section ) {
-//                Radical* r = [NSEntityDescription insertNewObjectForEntityForName:kEntityRadical inManagedObjectContext:self.managedObjectContext];
-//                r.isFirstRadical = [NSNumber numberWithBool:(sectionTally == 0 || sectionTally == 1)];
-//                    
-//                r.simplified = radical;
-//                r.position = [NSNumber numberWithInt:tally];
-//                r.section = [NSNumber numberWithInt:sectionTally];
-//                
-//                if([radical isEqualToString:@"人"]) {
-//                    int sectionTally2 = 0;
-//                    for(NSArray *section in  @[@[@"人", @"口", @"土", @"忄", @"日", @"纟", @"刂", @"⺮", @"车", @"方", @"冫", @"子", @"王", @"⺷", @"卩", @"爫", @"二", @"冂", @"ハ", @"⺈"],
-//                        @[@"心", @"手", @"氵", @"犭", @"马", @"十", @"方", @"饣", @"亠", @"艮", @"皿", @"示", @"小", @"止", @"牛", @"匕", @"凵", @"卜", @"毋", @"朩"]]) {
-//                        int tally2 = 0;
-//                        for(NSString *radical2 in section ) {
-//                            Radical* r2 = [NSEntityDescription insertNewObjectForEntityForName:kEntityRadical inManagedObjectContext:self.managedObjectContext];
-//                            r2.isFirstRadical = @NO;
-//                            r2.simplified = radical2;
-//                            r2.section = [NSNumber numberWithInt:sectionTally2];
-//                            r2.position = [NSNumber numberWithInt:tally2];
-//                            r2.firstRadical = r;
-//                            
-//                            if([radical2 isEqualToString:@"口"]) {
-//                                int cTally = 0;
-//                                for(NSString *character in @[@"喝", @"答", @"给", @"拿", @"容", @"命", @"合", @"拾", @"咳", @"盒", @"吸", @"俗"] ) {
-//                                     Character* c = [NSEntityDescription insertNewObjectForEntityForName:kEntityCharacter inManagedObjectContext:self.managedObjectContext];
-//                                     c.simplified = character;
-//                                     c.position = [NSNumber numberWithInt:cTally];
-//                                    
-//                                     [c addSecondRadicalsObject:r2];
-//                                    
-//                                     if([character isEqualToString:@"答"]) {
-//                                         int wTally = 0;
-//                                         for(NSArray *word in @[@[@"答", @"answer, reply; return; assent to"], @[@"报答", @"to repay; to requite"], @[@"答应", @"to promise; to agree; to reply"], @[@"答案", @"answer; solution;"], @[@"答辩", @"to reply (to an accusatio; to defend one's dissertat"], @[@"回答", @"to reply; to answer; the answer"]] ) {
-//                                             Word* w = [NSEntityDescription insertNewObjectForEntityForName:kEntityWord inManagedObjectContext:self.managedObjectContext];
-//                                             w.simplified = [word firstObject];
-//                                             w.english = [word lastObject];
-//
-//                                             w.position = [NSNumber numberWithInt:wTally];
-//                                             [w addCharactersObject:c];
-//                                         }
-//                                         wTally++;
-//                                         
-//                                     }
-//                                     cTally++;
-//
-//                                 }
-//
-//                             }
-//                             tally2++;
-//                        }
-//                        sectionTally2++;
-//                    
-//                    }
-//                    
-//                    // Assorted characters with ren:
-//                    int renTally = 0;
-//                    for(NSString *character in @[@"价", @"俩", @"俗", @"拾", @"擦", @"脸", @"输", @"茶"] ) {
-//                        Character* c = [NSEntityDescription insertNewObjectForEntityForName:kEntityCharacter inManagedObjectContext:self.managedObjectContext];
-//                        c.simplified = character;
-//                        c.position = [NSNumber numberWithInt:renTally];
-//                        
-//                        [c addSecondRadicalsObject:r]; // R is "isFirstRadical" but it is the secondRadical of these characters.
-//                        renTally++;
-//                    }
-//
-//                    
-//                }
-//                
-//                tally++;
-//            }
-//            sectionTally++;
-//        }
-//        
-//        int cTally = 0;
-//        
-//        for(NSString *character in @[@"推", @"赛", @"齐", @"黑", @"黄", @"鸽", @"鸟", @"鲜", @"鱼", @"马", @"码", @"首"]) {
-//            Character* c = [NSEntityDescription insertNewObjectForEntityForName:kEntityCharacter inManagedObjectContext:self.managedObjectContext];
-//            c.simplified = character;
-//            c.position = [NSNumber numberWithInt:cTally];
-//            cTally++;
-//        }
-//        
-//        NSError *error;
-//       [self.managedObjectContext save:&error];
-//        if(error != nil) {
-//          
-//        }
-//    }
-    
+    }    
     
     return YES;
 }
